f6f6f3c96191fd7c76b52eb60013313e221fc6cd
grijesh <gitu1992@gmail.com>
2020-06-16 14:45:53 +0800

    added sql directory

---
+++ sql/create_parcels.sql
@@ -0,0 +1,32 @@
+-- CREATE TABLE `parcels` (
+--   `id` varchar(255) NOT NULL,
+--   `username` varchar(255) NOT NULL,
+--   `note` varchar(4000) NOT NULL,
+--   `length` int,
+--   `breadth` int,
+--   `height` int,
+--   `weight` int,
+--   `category` varchar(255) NOT NULL,
+--   `src_address` varchar(2000) NOT NULL,
+--   `dest_address` varchar(2000) NOT NULL,
+--   `src_lat` DECIMAL(10, 8) NOT NULL,
+--   `src_long` DECIMAL(11, 8) NOT NULL,
+--   `dest_lat` DECIMAL(10, 8) NOT NULL,
+--   `dest_long` DECIMAL(11, 8) NOT NULL,
+--   `created_at` datetime NOT NULL,
+--   `price` int NOT NULL,
+--   `status` varchar(255) NOT NULL,
+--   `completed_at` datetime,
+--   PRIMARY KEY (`id`)
+-- );
+
+-- ALTER TABLE parcel_delivery.parcels
+-- ADD FOREIGN KEY (username) REFERENCES parcel_delivery.people(username);
+
+-- DROP TABLE parcel_delivery.parcels;
+
+-- SELECT * FROM parcel_delivery.parcels;
+
+-- DELETE FROM parcel_delivery.parcels;
+
+-- SELECT * FROM parcel_delivery.people;
---
+++ sql/create_tables.sql
@@ -0,0 +1,21 @@
+-- CREATE TABLE `travels` (
+--   `id` varchar(255) NOT NULL,
+--   `username` varchar(255) NOT NULL,
+--   `note` varchar(4000) NOT NULL,
+--   `mode` varchar(255),
+--   `src_address` varchar(2000) NOT NULL,
+--   `dest_address` varchar(2000) NOT NULL,
+--   `src_lat` DECIMAL(10, 8) NOT NULL,
+--   `src_long` DECIMAL(11, 8) NOT NULL,
+--   `dest_lat` DECIMAL(10, 8) NOT NULL,
+--   `dest_long` DECIMAL(11, 8) NOT NULL,
+--   `created_at` datetime NOT NULL,
+--   `status` varchar(255) NOT NULL,
+--   `completed_at` datetime,
+--   PRIMARY KEY (`id`)
+-- );
+
+-- ALTER TABLE parcel_delivery.travels
+-- ADD FOREIGN KEY (username) REFERENCES parcel_delivery.people(username);
+
+-- SELECT * FROM parcel_delivery.travels;
